c503472a7abeb1ccfa3005687bc62ba498a07ee8
DobsonDev <alex@dobsondev.com>
2025-05-07 08:31:29 -0600

    Using go-chi for middleware and 404 handling etc...

--- main.go
+++ main.go
@@ -4,20 +4,36 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/a-h/templ"
-
 	"github.com/dobsondev/gotth-stack/handlers"
 	"github.com/dobsondev/gotth-stack/templ/pages"
+
+	"github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+
+	// Serve static files
+	fileServer := http.FileServer(http.Dir("./static"))
+	router.Handle("/static/*", http.StripPrefix("/static/", fileServer))
 
 	page := pages.DemoPage()
-	http.Handle("/", templ.Handler(page))
+	router.Handle("/", templ.Handler(page))
 
-	http.HandleFunc("/api/hello", handlers.HelloHandler)
+	router.HandleFunc("/api/hello", handlers.HelloHandler)
+
+	server := http.Server{
+		Addr:    ":3000",
+		Handler: router,
+	}
 
 	fmt.Println("App running on :3000")
-	http.ListenAndServe(":3000", nil)
+	server.ListenAndServe()
 }
